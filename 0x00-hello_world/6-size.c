5c4ebdabebe845441939db03ec36d45fbc9dfc51
georgekaunda <godknowskaunda@gmail.com>
2022-09-08 14:06:23 -0700

    tshbg6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 /**
-*main - print the string in the put function
+*main - Prints the size of many var types
 *
-*Description - using the main function
-*this program that prints the size of various types on the computer it is compiled and run on
-*Return: 0
+*Return: Always 0 (Success)
 */
 int main(void)
 {
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
 
-	printf("Size of a char:%ld byte(s)\n", sizeof(c));
-	printf("Size of an int:%ld byte(s)\n", sizeof(i));
-	printf("Size of a ling int:%ld byte(s)\n", sizeof(li));
-	printf("Size of a long long int:%ld byte(s)\n", sizeof(lli));
-	printf("Size of a float:%ld byte(s)\n", sizeof(f));
+	printf("Size of a char:%d byte(s)\n", sizeof(a));
+	printf("Size of an int:%d byte(s)\n", sizeof(b));
+	printf("Size of a long int:%d byte(s)\n", sizeof(c));
+	printf("Size of a long long int:%d byte(s)\n", sizeof(d));
+	printf("Size of a float:%d byte(s)\n", sizeof(e));
 	return (0);
 }
