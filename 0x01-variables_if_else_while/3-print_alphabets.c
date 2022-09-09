c7cd74501e421bf4bb95715b1a20b5753103916d
georgekaunda <godknowskaunda@gmail.com>
2022-09-09 06:47:42 -0700

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main- prints the alphabet in lower and upper
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 Positive anything is better than negative nothing
 The last digit
 I sometimes suffer from insomnia. And when I cant fall asleep, I play what I call the alphabet game
+a program that prints the alphabet in lowercase, and then in uppercase, followed by a new line.
