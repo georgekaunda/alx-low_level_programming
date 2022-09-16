0a4f26966d35d6cdc44876af058d0502a5bfed4a
georgekaunda <godknowskaunda@gmail.com>
2022-09-15 21:46:02 -0700

    task101

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -19,12 +19,10 @@ void print_triangle(int size)
 			{
 				putchar(' ');
 			}
-			
 			for (inc2 = 0; inc2 < inc1; inc2++)
 			{
 				putchar('#');
 			}
-
 			if (inc1 == size)
 			{
 				continue;
