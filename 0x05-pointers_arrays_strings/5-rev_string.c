b3da0e5b048a2f1cd29d80e9cbc98b8bdb4ab46f
georgekaunda <godknowskaunda@gmail.com>
2022-09-19 04:05:26 -0700

    TAsk5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+
+void rev_string(char *s)
+{
+	int i, j, k, temp;
+
+	i= 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	k = 0;
+	j =i-1;
+
+	while (k<j)
+	{
+		temp = s[k];
+		s[k] = s[j];
+		s[j] = temp;
+		k++;
+		j--;
+	}
+}
