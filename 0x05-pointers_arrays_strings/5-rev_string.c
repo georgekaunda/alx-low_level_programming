493bdb704d9bdf41aff40c323d8e302f5fcac109
georgekaunda <godknowskaunda@gmail.com>
2022-09-19 04:22:04 -0700

    Task6

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,15 +11,15 @@ void rev_string(char *s)
 {
 	int i, j, k, temp;
 
-	i= 0;
+	i = 0;
 	while (s[i] != '\0')
 	{
 		i++;
 	}
 	k = 0;
-	j =i-1;
+	j = i - 1;
 
-	while (k<j)
+	while (k < j)
 	{
 		temp = s[k];
 		s[k] = s[j];
